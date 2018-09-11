1afacd9ee0598b5497fdccc7cbe69d37827c2ff4
Ashutosh Jadhav <ashutoshjadhav661@gmailcom>
2018-09-11 18:38:03 +0530

    New HR

--- py/hello.py
+++ py/hello.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
+
 print('hello ')
 x=input()
 print(x)
-||||||| merged common ancestors
-=======
-print('hello')
-x=input()
-print(x)
->>>>>>> ab91b3e29fa89d0eda57ed186420ce112a446f0e
