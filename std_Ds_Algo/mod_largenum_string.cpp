b0a28445457e2ed79028e3bdd2b6959359db5da4
Ashutosh Jadhav <31470393+ashutoshjv661@users.noreply.github.com>
2019-07-15 23:29:28 +0530

    modLargeNumString

---
+++ std_Ds_Algo/mod_largenum_string.cpp
@@ -0,0 +1,21 @@
+#include<iostream> 
+using namespace std; 
+
+// Function to compute num (mod a) 
+int mod(string num, int a) 
+{ 
+	// Initialize result 
+	int res = 0; 
+	// One by one process all digits of 'num' 
+	for (int i = 0; i < num.length(); i++) 
+		res = (res*10 + (int)num[i] - '0') %a; 
+
+	return res; 
+} 
+
+int main() 
+{ 
+	string num = "12316767678678"; 
+	cout << mod(num, 10); 
+	return 0; 
+} 
