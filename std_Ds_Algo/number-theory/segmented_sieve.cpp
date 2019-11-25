6247933c19d2bc00c54fafc395225aaa4b5804f7
Ashutosh Jadhav <ashutoshjadhav661@gmail.com>
2019-11-25 23:12:35 +0530

    Sieve added along with segmented sieve

---
+++ std_Ds_Algo/number-theory/segmented_sieve.cpp
@@ -0,0 +1,73 @@
+/*
+Segmented Sieve 
+When you need to find large prime numbers which are of the range 10^6 and more because 
+we cannot create a sieve of that size so we see that at least the difference between the 
+two big numbers is less than 10^6
+so that we can create a array of 10^6 and use segmented sieve
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+// Coding ninjas solution
+#define MAX 100001
+vector<int>sieve(){
+    bool isPrime[MAX];
+    for(int i=0;i<MAX;i++)isPrime[i]=1;
+
+for(int i=2;i*i<MAX;i++){
+    if(isPrime[i]){
+        for(int j=i*i;j<MAX;j+=i){
+            isPrime[j]=false;
+        }
+    }
+}
+
+vector<int> primes;
+primes.push_back(2);
+for(int i=3;i<MAX;i+=2){
+    if(isPrime[i]){
+        primes.push_back(i);
+    }
+}
+return primes;
+
+}
+void printPrimes(long long l,long long r,vector<int> primes){
+    bool isPrime[r-l+1];
+    // 0th index lth index nth index is rth index 
+    for(int i=0;i<=r-l;i++){
+        isPrime[i]=true;
+    }
+    // n/i+i is actually floor() of n/i
+    for(int i=0;primes[i]*(long long )primes[i]<=r;i++){
+        int currPrime=primes[i];
+        // just smaller of equal to r
+        long long base = (l/(currPrime))*(currPrime);
+        if(base<l){
+            base=base+currPrime;
+        }
+        // Mark all multiples within L to R as false 
+        for(long long j=base;j<=r;j+=currPrime){
+            isPrime[j-l]=false;
+        }
+        // There may be a case where base is itself prime because base evaluated as 0
+        if(base==currPrime){
+            isPrime[base-l] = true ;
+        }
+    }
+    for(int i=0;i<=r-l;i++){
+        if(isPrime[i]== true){
+            cout<< i + l ;
+        }
+    }
+}
+int main(){
+    vector<int>primes = sieve();
+    int t;
+    cin>>t;
+    while(t--){
+        long long l,r;
+        cin>>l>>r;
+        printPrimes(l,r,primes);
+    }
+}
---
+++ std_Ds_Algo/number-theory/seive.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void prime_seive(int * p,int n){
+    
+    for(int i=3;i<=n;i+=2){
+        p[i]=1;
+    }
+    p[0]=p[1]= 0;
+    p[2]=1;
+    // first optimisation is that iterate only over the odd numbers
+    for(int i=3;i*i<=n;i+=2){
+        if(p[i]){
+            // 2nd optimisation is that we are starting from i^2 to check for next non-prime 
+            for(int j=i*i;j<=n;j+=2*i){ // another optimisation is that we are doing j+= 2i because we are skipping even number 
+            // odd + odd is even so we are doing odd + even every time 
+                p[j]=0;
+            }
+        }
+    }
+    return ;
+}
+int main(){
+
+    int N=1000000;
+    int p[N];
+    memset(p,0,sizeof(p));
+    prime_seive(p,10);
+}
