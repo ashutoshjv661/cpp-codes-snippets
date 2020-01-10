14b9f2e91e67aa995590c1c7a9b96f494207d84a
Ashutosh Jadhav <ashutoshjadhav661@gmail.com>
2020-01-10 18:28:48 +0530

    Linkedlist-file moved

--- std_Ds_Algo/Datastructures/LinkedList/linked-list.cpp
+++ std_Ds_Algo/Datastructures/LinkedList/linked-list.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 using namespace std;
-#define OJ freopen("input.txt","r",stdin);
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define PB push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
 
 class Linked
 {
@@ -14,7 +33,7 @@ Linked *createNode(int value)
 {
     Linked *p = NULL;
     p = new Linked();
-    p->data = value;
+    p->data = value;                          
     p->next = NULL;
     return p;
 }
