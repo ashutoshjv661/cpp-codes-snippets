f4f71a6fbf56da74a492b37baa624e46a87670b2
Ashutosh Jadhav <ashutoshjadhav661@gmail.com>
2020-01-20 16:53:22 +0530

    linked list modification

--- std_Ds_Algo/Datastructures/LinkedList/linked-list.cpp
+++ std_Ds_Algo/Datastructures/LinkedList/linked-list.cpp
@@ -1,20 +1,6 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-typedef vector <int> vi;
-typedef pair< int ,int > pii;
 #define endl "\n"
-#define sd(val) scanf("%d",&val)
-#define ss(val) scanf("%s",&val)
-#define sl(val) scanf("%lld",&val)
-#define debug(val) printf("check%d\n",val)
-#define all(v) v.begin(),v.end()
-#define PB push_back
-#define MP make_pair
-#define FF first
-#define SS second
-#define ll long long
-#define MOD 1000000007
-#define clr(val) memset(val,0,sizeof(val))
 #define OJ \
     freopen("input.txt", "r", stdin); \
     freopen("output.txt", "w", stdout);
@@ -24,8 +10,8 @@ typedef pair< int ,int > pii;
 class Linked
 {
     public: 
-    int data;
-    Linked* next;
+        int data;
+        Linked* next;
 };
 Linked *head = NULL;
 Linked *tail = NULL;
