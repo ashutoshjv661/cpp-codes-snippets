20c6e2b877193234e809c98e53bc5751ba932d6b
Ashutosh Jadhav <ashutoshjadhav661@gmail.com>
2022-03-01 22:40:35 +0530

    new cpp file

---
+++ snippet/new.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define ll long long
+#define what_is(x) cerr << #x << " is " << x << endl; 
+#define tc ll T;cin>>T;while(T--)
+void oj() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+int main() 
+{
+  FIO;
+  oj();
+  
+
+
+
+return 0;
+}
